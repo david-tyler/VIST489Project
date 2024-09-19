ad18beff68e43084b1e550aa9c1f43d14d94b424
David-Tyler Ighedosa <ighedosadt@gmail.com>
2024-09-18 23:28:44 -0500

    Made changes to world game behavior with buttons

--- VIST489_Project/Assets/Scripts/GameSystemBehavior.cs
+++ VIST489_Project/Assets/Scripts/GameSystemBehavior.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class GameSystemBehavior : MonoBehaviour
 {
-    public GameObject pkWorldBttn;
-    private ParaLensesButtonBehavior paraLenses;
+   
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        paraLenses = this.GetComponent<ParaLensesButtonBehavior>();
-
+        
     }
 
     // Update is called once per frame
--- VIST489_Project/Assets/Scripts/ParaLensesButtonBehavior.cs
+++ VIST489_Project/Assets/Scripts/ParaLensesButtonBehavior.cs
@@ -5,20 +5,59 @@ using UnityEngine;
 public class ParaLensesButtonBehavior : MonoBehaviour
 {
     private bool isParaLensesOn = false;
+    private bool inPokemonWorld = false;
+    private string disableLenses = "Disable Paranormal Lenses";
+    private string enableLenses = "Enable Paranormal Lenses";
+    private string LeavePKWorld = "Leave Pokemon World";
+    private string enterPKWorld = "Enter Pokemon World";
+
 
     public GameObject enterPkWorldButton;
     public GameObject ImageTargetCharSitting;
     public GameObject ImageTargetPKAudio;
+    public TMPro.TextMeshProUGUI ParaLenses;
+    public TMPro.TextMeshProUGUI PkWorldBttnText;
+    private void Start()
+    {
+        ImageTargetCharSitting.SetActive(false);
+        ImageTargetPKAudio.SetActive(false);
+    }
 
     public void ShowEnterPkWorldButton()
     {
         isParaLensesOn = !isParaLensesOn;
 
+        if (isParaLensesOn)
+        {
+            ParaLenses.text = disableLenses;
+        }
+        else
+        {
+            ParaLenses.text = enableLenses;
+        }
+
 
         enterPkWorldButton.SetActive(isParaLensesOn);
-        ImageTargetCharSitting.SetActive(isParaLensesOn);
-        ImageTargetPKAudio.SetActive(isParaLensesOn);
+        
     }
+
+    public void EnterPKWorldButtonClicked()
+    {
+        inPokemonWorld = !inPokemonWorld;
+
+        if (inPokemonWorld)
+        {
+            PkWorldBttnText.text = LeavePKWorld;
+        }
+        else
+        {
+            PkWorldBttnText.text = enterPKWorld;
+        }
+
+        ImageTargetCharSitting.SetActive(inPokemonWorld);
+        ImageTargetPKAudio.SetActive(inPokemonWorld);
+    }
+
 
     public bool getIsParaLensesOn()
     {
