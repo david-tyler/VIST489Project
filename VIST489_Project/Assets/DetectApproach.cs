3df90b030414c1e60356ebbb3b436888737cb626
Morgan Sinko <msinko@axon.com>
2024-10-03 08:00:04 -0700

    fixed Door prompt bug

--- VIST489_Project/Assets/DetectApproach.cs
+++ VIST489_Project/Assets/DetectApproach.cs
@@ -6,17 +6,23 @@ public class DetectApproach : MonoBehaviour
 {
     PopUpSystem popUp;
 
+    public bool fired = false;
     public GameObject YouNeedToFindTheDoorPopUp;
     public TMPro.TextMeshProUGUI text_YouNeedToFindTheDoorPopUp;
     public Animator YouNeedToFindTheDoorPopUpAnimator;
 
     void OnCollisionEnter(Collision collision)
     {
-        popUp = PopUpSystem.instance;
-        popUp.popUpBox = YouNeedToFindTheDoorPopUp;
-        popUp.popUpText = text_YouNeedToFindTheDoorPopUp;
-        popUp.animator = YouNeedToFindTheDoorPopUpAnimator;
-        popUp.PopUp(text_YouNeedToFindTheDoorPopUp.text);
+        Debug.Log("collided");
+        if(!fired)
+        {
+            popUp = PopUpSystem.instance;
+            popUp.popUpBox = YouNeedToFindTheDoorPopUp;
+            popUp.popUpText = text_YouNeedToFindTheDoorPopUp;
+            popUp.animator = YouNeedToFindTheDoorPopUpAnimator;
+            popUp.PopUp(text_YouNeedToFindTheDoorPopUp.text);
+        }
+
+        fired = true;
     }
-    
 }
