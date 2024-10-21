233f39ba3a715440cdaa1d50fac8401bddd46589
Morgan Sinko <msinko@axon.com>
2024-10-20 19:35:05 -0700

    Adding in debug positional resets, renaming calibration coroutine

--- VIST489_Project/Assets/DebugViewer.cs
+++ VIST489_Project/Assets/DebugViewer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DebugViewer : MonoBehaviour
 {
     public List<Renderer> modelsToToggle = new List<Renderer>();
+    public List<GameObject> UIToToggle = new List<GameObject>();
     public bool debugOn = false;
 
     private void Start()
@@ -31,6 +32,28 @@ public class DebugViewer : MonoBehaviour
             for (int i = 0; i < modelsToToggle.Count; i++)
             {
                 modelsToToggle[i].enabled = true;
+            }
+
+            debugOn = true;
+        }
+    }
+
+    public void ToggleUI()
+    {
+        if (debugOn == true)
+        {
+            for (int i = 0; i < UIToToggle.Count; i++)
+            {
+                UIToToggle[i].SetActive(false);
+            }
+
+            debugOn = false;
+        }
+        else
+        {
+            for (int i = 0; i < modelsToToggle.Count; i++)
+            {
+                UIToToggle[i].SetActive(true);
             }
 
             debugOn = true;
