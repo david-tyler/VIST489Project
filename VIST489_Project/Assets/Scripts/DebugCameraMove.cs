fe56fe6b5219fbd78da8de6c5fdf9a5f96490ab7
Morgan Sinko <msinko@axon.com>
2024-11-04 22:27:26 -0800

    Fixed Debug buttons with resetpose

--- VIST489_Project/Assets/Scripts/DebugCameraMove.cs
+++ VIST489_Project/Assets/Scripts/DebugCameraMove.cs
@@ -7,11 +7,38 @@ public class DebugCameraMove : MonoBehaviour
     public GameObject ARCamera;
 
     public Transform positionToGo;
-    
+
 
     public void DebugMove()
     {
-        ARCamera.transform.position = positionToGo.position;
-        ARCamera.transform.rotation = positionToGo.rotation;
+        //ARCamera.transform.position = positionToGo.position;
+        //ARCamera.transform.rotation = positionToGo.rotation;
+
+        // GameObject arCameraParent = new GameObject("ARCameraParent");
+        //arCameraParent.transform.position = positionToGo.position; //desired position
+        ///arCameraParent.transform.rotation = positionToGo.rotation; //desired position
+        //ARCamera.transform.parent = arCameraParent.transform;
+
+
+
+
+        Vuforia.VuforiaBehaviour.Instance.DevicePoseBehaviour.enabled = false;
+        ARCamera.transform.position = positionToGo.position; // Set desired position
+        ARCamera.transform.rotation = positionToGo.rotation; // Set desired position
+                                                             // Re-enable tracking when done
+        Vuforia.VuforiaBehaviour.Instance.DevicePoseBehaviour.RecenterPose();
+        //Vuforia.VuforiaBehaviour.Instance.DevicePoseBehaviour.Reset();// = true;
+        Vuforia.VuforiaBehaviour.Instance.DevicePoseBehaviour.enabled = true;
+
+        //StartCoroutine(EnableTracking());
+        
+    }
+
+    public IEnumerator EnableTracking()
+    {
+        yield return new WaitForSeconds(5);
+        print("WaitAndPrint " + Time.time);
+        Vuforia.VuforiaBehaviour.Instance.DevicePoseBehaviour.Reset();// = true;
+        Vuforia.VuforiaBehaviour.Instance.DevicePoseBehaviour.enabled = true;
     }
 }
